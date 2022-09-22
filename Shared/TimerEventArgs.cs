dd2680a28487f400966984630e0dc6dbfde2c7d6
a.akimov <akimov@dostek.kg>
2022-09-22 17:28:47 +0600

    using events

---
+++ Shared/TimerEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.WasmSample.Shared;
+
+public class TimerEventArgs : EventArgs
+{
+    public DateTime SignalTime { get; set; }
+}
