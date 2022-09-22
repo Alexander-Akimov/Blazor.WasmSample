27a0402b405add14d720355356ff88956d1abb62
akimov <akimov.infotech@gmail.com>
2022-09-22 09:17:02 +0600

    using service

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,7 @@
-using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazor.WasmSample.Client;
+using Blazor.WasmSample.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { 
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
 });
-
+builder.Services.AddScoped<CounterService>();
 await builder.Build().RunAsync();
