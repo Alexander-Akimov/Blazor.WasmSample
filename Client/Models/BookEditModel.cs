6e23e75c5c63d88a389b29ad23e62410f5ca6778
akimov <akimov.infotech@gmail.com>
2022-09-25 19:17:22 +0600

    passing parameters to components, injecting services, using event callbacks, programmatically updating the user interface, two-way binding, cascading parameters, templated components, and built-in components.

---
+++ Client/Models/BookEditModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor.WasmSample.Client.Models;
+
+public class BookEditModel
+{
+    [StringLength(20, ErrorMessage = "Title is too long")]
+    [Required]
+    public string Title { get; set; } = string.Empty;
+    public DateTime ReleaseDate { get; set; } = DateTime.Today;
+    public string? Type { get; set; } = string.Empty;
+}
---
+++ Client/Services/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazor.WasmSample.Client.Services;
+
+public class Book
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Publisher { get; set; } = string.Empty;
+    public DateTime ReleaseDate { get; set; }
+
+}
